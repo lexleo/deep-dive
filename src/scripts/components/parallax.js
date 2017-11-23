1c7eca08cbe031b08c9ebd4d674b198d54d60d6b
lexleo <ins227@gmail.com>
2017-11-23 21:19:21 +0300

    hangar parallax

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -2,6 +2,7 @@ import Parallax from 'parallax-js';
 
 document.addEventListener("DOMContentLoaded", (e) => {
     let parallax = document.getElementById('parallax');
+    console.log(parallax);
     var parallaxInstance = new Parallax(parallax);
 });
 
