6f1b022ef9f5d7272814f4f8593f1db2330e6c09
Alexey Shilyakin <ins227@gmail.com>
2017-11-24 16:28:34 +0300

    parallax step 2

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -3,7 +3,9 @@ import Parallax from 'parallax-js';
 document.addEventListener("DOMContentLoaded", (e) => {
     let parallax = document.getElementById('parallax');
     console.log(parallax);
-    var parallaxInstance = new Parallax(parallax);
+    var parallaxInstance = new Parallax(parallax, {
+        clipRelativeInput: true
+    });
 });
 
 
