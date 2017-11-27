28620a568ae3eec3c4bf6fc28a8dfcedf857c528
lexleo <ins227@gmail.com>
2017-11-27 08:15:18 +0300

    parallax another step

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -4,7 +4,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
     let parallax = document.getElementById('parallax');
     console.log(parallax);
     var parallaxInstance = new Parallax(parallax, {
-        clipRelativeInput: true
+        relativeInput: false,
+        clipRelativeInput: true,
+        frictionX: 0.05,
+        frictionY: 0.05,
+        limitY: false,
+        limitX: false,
+        scalarX: 10,
+        scalarY: 3
     });
 });
 
