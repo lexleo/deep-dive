81cafc70a69f665fb0b16fc8f981d9464b55769d
Alexey Shilyakin <ins227@gmail.com>
2017-11-29 16:46:10 +0300

    parallax big

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -4,15 +4,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
     let parallax = document.getElementById('parallax');
     console.log(parallax);
     var parallaxInstance = new Parallax(parallax, {
-        relativeInput: false,
-        clipRelativeInput: true,
-        frictionX: 0.1,
-        frictionY: 0.1,
-        limitY: 5,
-        limitX: 5,
-        scalarX: 10,
-        scalarY: 5,
-        originY: 0.8,
+        // relativeInput: false,
+        // clipRelativeInput: true,
+        // frictionX: 0.1,
+        // frictionY: 0.1,
+        scalarX: 20,
+        scalarY: 10,
+        // originY: 0.8,
     });
 });
 
