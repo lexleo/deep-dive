48f72685b143542b15ca0dbd5b4156a3d015a755
Alexey Shilyakin <ins227@gmail.com>
2017-12-07 17:03:22 +0300

    parallax next level

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -1,21 +1,25 @@
 import Parallax from 'parallax-js';
+var images;
 
 document.addEventListener("DOMContentLoaded", (e) => {
     let parallax = document.getElementById('parallax');
-    console.log(parallax);
+    images = document.querySelectorAll('.parallax__image');
+
+    manageParallax();
+
     var parallaxInstance = new Parallax(parallax, {
         relativeInput: true,
         clipRelativeInput: true,
         // frictionX: 0.1,
         // frictionY: 0.1,
 
-        // scalarX: 40,
+        scalarX: 20,
         // scalarY: 10,
 
         // originY: 0.8,
         // originX: 0
-        limitX: 500,
-        limitY: 0
+        // limitX: 700,
+        limitY: 50
     });
 });
 
@@ -24,5 +28,32 @@ if (window.DeviceOrientationEvent) {
         location.reload();
     }, false);
 }
+
+
+function manageParallax() {
+
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    
+    if ((width > height) && (images[0].classList.contains('image-h'))) {
+        alert('h to w!');
+        images.forEach(function(el) {
+            el.classList.remove('image-h');
+            el.classList.add('image-w');
+        });
+    }
+
+    if ((width < height) && (images[0].classList.contains('image-w'))) {
+        alert('w to h!');
+        images.forEach(function(el) {
+            el.classList.remove('image-w');
+            el.classList.add('image-h');
+        });
+    }
+}
+
+window.onresize(manageParallax());
+
+
 
 
